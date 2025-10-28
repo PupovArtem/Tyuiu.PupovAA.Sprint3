ac4a0de6bf338fdbf4c3da2567aad4df352d0afb
Artem Pupov <Tyuiu.PupovAA@outlook.com>
2025-10-28 23:46:25 +0500

    Task1

--- Tyuiu.PupovAA.Sprint3.Task1.V7.Lib/DataService.cs
+++ Tyuiu.PupovAA.Sprint3.Task1.V7.Lib/DataService.cs
@@ -6,10 +6,10 @@ namespace Tyuiu.PupovAA.Sprint3.Task1.V7.Lib
     {
         public double GetMultiplySeries(double value, int startValue, int stopValue)
         {
-            double p = 0;
+            double p = 1;
             while (startValue <= stopValue )
             {
-                p = p +  ((Math.Pow(value, startValue)+1)*Math.Sin(5));
+                p = p *  ((Math.Pow(value, startValue)+1)*Math.Sin(5));
                 startValue++;
 
             }
