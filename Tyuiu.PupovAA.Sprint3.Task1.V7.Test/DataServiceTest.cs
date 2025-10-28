4cfee1bbdf7e17050d91cad399f394f3832831d4
Artem Pupov <Tyuiu.PupovAA@outlook.com>
2025-10-28 23:27:36 +0500

    Task1

---
+++ Tyuiu.PupovAA.Sprint3.Task1.V7.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.PupovAA.Sprint3.Task1.V7.Lib;
+namespace Tyuiu.PupovAA.Sprint3.Task1.V7.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double a = 0.25;
+            int k = 1;
+            int k2 = 9;
+
+            var result = ds.GetMultiplySeries(a, k, k2);
+            Assert.AreEqual(-8.950, result);
+            
+               
+        }
+    }
+}
---
+++ Tyuiu.PupovAA.Sprint3.Task1.V7/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.PupovAA.Sprint3.Task1.V7.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #0 | Выполнил: Пупов А. А. | ИБКСб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #3                                                               *");
+        Console.WriteLine("* Тема: Оператор цикла while                                               *");
+        Console.WriteLine("* Задание #1                                                              *");
+        Console.WriteLine("* Вариант #7                                                              *");
+        Console.WriteLine("* Выполнил: Пупов Артём Андреевич | ИБКСб-25-1                            *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу используя цикл while, которая вычисляет               *");
+        Console.WriteLine("* произведение ряда по формуле, при a=0,25                               *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                      *");
+        Console.WriteLine("***************************************************************************");
+
+        double a = 0.25;
+        int k = 1;
+        int k2 = 9;
+
+        var result = ds.GetMultiplySeries(a, k, k2);
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("P = "+result);
+        Console.ReadLine();
+    }
+}
